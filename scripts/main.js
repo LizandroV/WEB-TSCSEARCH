97b83a382db13bccebbffe9997feeaad62a98b53
LizandroV <lizandrovivanco16@gmail.com>
2024-04-12 18:30:33 -0500

    adding search with enter

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
-// copyright year
+/////////// COPYRIGHT YEAR BUTTON  ///////////
 document.querySelector('#year').textContent = new Date().getFullYear();
 
-// toggle menu in small view
+/////////// TOGGLE BUTTON - MENU ///////////
 const toggleMenu = () => {
     document.querySelector('#menu').classList.toggle('open');
 }
-
 document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 
+
+/////////// SEARCH BUTTON  ///////////
+document.getElementById('idInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        document.getElementById('searchbuttom').click();
+    }
+});
