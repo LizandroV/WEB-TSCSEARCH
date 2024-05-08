a9cb096ab31c93a37a0636b05d2ff1f4f1f5cd41
LizandroV <lizandrovivanco16@gmail.com>
2024-05-08 16:07:18 -0500

    adding password to mail and printer codes

---
+++ scripts/auth.js
@@ -0,0 +1,20 @@
+function checkAuthentication() {
+    var authenticated = document.cookie.indexOf("authenticated=true") !== -1;
+    if (!authenticated) {
+        promptForPassword();
+    }
+}
+
+// Función para solicitar la contraseña
+function promptForPassword() {
+    var password = prompt("Ingrese la contraseña para acceder:");
+    if (password === "pass") { // Reemplaza "tu_contraseña" con tu contraseña real
+        document.cookie = "authenticated=true; path=/"; // Establece la cookie de autenticación
+    } else {
+        alert("Contraseña incorrecta. Inténtelo de nuevo.");
+        promptForPassword(); // Solicita la contraseña nuevamente si es incorrecta
+    }
+}
+
+// Verificar la autenticación al cargar la página
+window.onload = checkAuthentication;
